e7cbc551118273dd85c310c8c3e81cac74b7215e
Sesiunina Olga <sesyunina.olga7@gmail.com>
2024-03-19 21:11:46 +0300

    completed stage 4 (40)

--- tests/fixtures/gendiff_test.py
+++ tests/fixtures/gendiff_test.py
@@ -12,7 +12,12 @@ result = "{\n\
 file1 = 'tests/fixtures/file1.json'
 file2 = 'tests/fixtures/file2.json'
 
-if generate_diff(file1, file2) != result:
-    raise Exception('Функция работает не верно.')
 
-print('Тесты пройдены!')
+def test():
+    if generate_diff(file1, file2) != result:
+        raise Exception('Функция работает не верно.')
+    print('Тесты пройдены!')
+    return 'Тесты пройдены!'
+
+
+test()

659ecbb08c8a79e73cd58c2d2444d83646f4dcd8
Sesiunina Olga <sesyunina.olga7@gmail.com>
2024-05-08 14:01:14 +0300

    stage 8(4)

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,16 +1,16 @@
 import json
-import gendiff.consts
+# import gendiff.consts
 
 
 def json_form(data):
-    for value in data.values():
-        match value['type']:
-            case gendiff.consts.DELETED:
-                value['type'] = '-'
-            case gendiff.consts.ADDED:
-                value['type'] = '+'
-            case gendiff.consts.CHANGED:
-                value['type'] = '-+'
-            case gendiff.consts.UNCHANGED:
-                value['type'] = ' '
+    # for value in data.values():
+    #     match value['type']:
+    #         case gendiff.consts.DELETED:
+    #             value['type'] = '-'
+    #         case gendiff.consts.ADDED:
+    #             value['type'] = '+'
+    #         case gendiff.consts.CHANGED:
+    #             value['type'] = '-+'
+    #         case gendiff.consts.UNCHANGED:
+    #             value['type'] = ' '
     return json.dumps(data)

049d80cfbeb9577d91f08281e590f42a1a417cc7
Sesiunina Olga <sesyunina.olga7@gmail.com>
2024-05-16 19:16:05 +0300

    corrected based on teacher comments(8)

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -1,6 +1,5 @@
 from gendiff.gendiff import generate_diff
 import pytest
-# import os
 
 
 PATH_FILE1_YAML = 'tests/fixtures/file1.yml'
@@ -51,10 +50,3 @@ def test_gendiff(path_file1, path_file2, check_file_path, format):
 
     with open(check_file_path) as check_file:
         assert result == check_file.read()
-
-# def get_fixtures_path(file_name):
-#     current_dir = os.path.dirname(__file__)
-#     return os.path.join(current_dir, 'fixtures', file_name)
-# def get_data(expected_result):
-#     with open(get_fixtures_path(expected_result), "r") as correct:
-#         return correct.read()

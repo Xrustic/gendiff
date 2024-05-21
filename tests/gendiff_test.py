baadf66f99645b6bb5ebc1404cf3cff6b74e251f
Sesiunina Olga <sesyunina.olga7@gmail.com>
2024-05-21 20:47:00 +0300

    fixed all(4)

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -8,8 +8,8 @@ def get_fixtures_path(file_name):
     return os.path.join(current_dir, 'fixtures', file_name)
 
 
-def get_data(expected_result):
-    with open(get_fixtures_path(expected_result), "r") as correct:
+def get_data(exp_result):
+    with open(get_fixtures_path(exp_result), "r") as correct:
         return correct.read()
 # PATH_FILE1_YAML = 'tests/fixtures/file1.yml'
 # PATH_FILE2_YAML = 'tests/fixtures/file2.yml'
@@ -61,7 +61,7 @@ def test_gendiff(file1, file2):
     path1 = get_fixtures_path(file1)
     path2 = get_fixtures_path(file2)
 
-    assert generate_diff(path1, path2, 'stylish') == get_data('expected_stylish')
-    assert generate_diff(path1, path2, 'plain') == get_data('expected_plain')
-    assert generate_diff(path1, path2, 'json') == get_data('expected_json')
-    assert generate_diff(path1, path2) == get_data('expected_stylish')
+    assert generate_diff(path1, path2, 'stylish') == get_data('exp_stylish')
+    assert generate_diff(path1, path2, 'plain') == get_data('exp_plain')
+    assert generate_diff(path1, path2, 'json') == get_data('exp_json')
+    assert generate_diff(path1, path2) == get_data('exp_stylish')
